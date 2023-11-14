200bc72c423ba3ec5840e96abe6d8dfe623aaf82
Daniel <hysiu07@gmail.com>
2023-11-14 19:09:09 +0100

    api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-
+'use client';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Main from './components/layouts/Main';
 export default function Home() {
+	const [name, setName] = useState();
+	useEffect(() => {
+		fetch('http://localhost:3000/api/hello')
+			.then((res) => res.json())
+			.then((data) => setName(data.name));
+	}, []);
 	return (
 		<Main>
 			<h1>Witam w NextJS</h1>
 			<Link href='/another'>Dalej</Link>
+			<p>{name}</p>
 		</Main>
 	);
 }
